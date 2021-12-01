0935339af89748b97f3c1b0d5ccc9af5dc854feb
Damir <damir@celion.ru>
2021-12-01 14:51:34 +0500

    User schema

--- src/api/users/users.model.js
+++ src/api/users/users.model.js
@@ -1,9 +1,14 @@
 const { Model } = require('objection');
+const schema = require('./users.schema.json');
 const { tableNames } = require('../../constants/tableNames');
 
 class Users extends Model {
   static get tableName() {
     return tableNames.users;
+  }
+
+  static get jsonSchema() {
+    return schema;
   }
 }
 
